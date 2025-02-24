789bd624f72b11a25c4ed6d224faebb2e0873b51
sharon2pan <pan.sha@northeastern.edu>
2025-02-24 11:31:11 -0500

    First implementation of gumball machine puzzle
    commit c49b34d1d38b175afaac5d15afcd1bce73dae9fc
    Author: sharon2pan <pan.sha@northeastern.edu>
    Date:   Mon Feb 24 04:28:38 2025 -0500
    
        gumball machine puzzle first implementation, with sfx
    
    commit f4552cf194bcbd7a810aace0ddc43e94846c4a70
    Author: sharon2pan <pan.sha@northeastern.edu>
    Date:   Fri Feb 21 13:48:12 2025 -0500
    
        start of inventory system
    
    commit af9c0603727c2111ab3e2fd174b4577978c28a96
    Author: marcellalmiller <149106160+marcellalmiller@users.noreply.github.com>
    Date:   Sat Feb 15 18:27:44 2025 -0500
    
        Milestone 2 puzzles added
    
        Bookshelf and gumball machine added to scene, prefabs created for bookshelf, gumball machine, gumball machine coin, and 2 keys

---
+++ RestEasy/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    public Camera mainCamera;
+    public AudioClip coinInDispenserSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+
+            Ray ray = mainCamera.ScreenPointToRay(mousePos);
+
+            RaycastHit raycastHit;
+
+            bool hit = Physics.Raycast(ray, out raycastHit);
+
+            Debug.Log(raycastHit.transform.name);
+            if (hit && raycastHit.transform.gameObject.tag == "Collectible") 
+            {
+                raycastHit.transform.gameObject.GetComponentInChildren<ICollectible>().Collect();
+            }
+            else if (hit && raycastHit.transform.gameObject.tag == "GumballMachine")
+            {
+                AudioSource.PlayClipAtPoint(coinInDispenserSFX, Camera.main.transform.position);
+                GumballPuzzle gumballPuzzle = raycastHit.transform.gameObject.GetComponent<GumballPuzzle>();
+                gumballPuzzle.CompleteGumballPuzzle();
+            }
+            else
+            {
+                Debug.Log("Did not hit anything :(");
+            }
+        }
+    }
+}
