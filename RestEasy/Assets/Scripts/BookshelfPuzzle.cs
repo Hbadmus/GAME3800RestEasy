c3f2390abeb61eeb08a20d1638af0eb981a411cc
sharon2pan <pan.sha@northeastern.edu>
2025-03-17 17:45:16 -0400

    progress with bookshelf puzzle
    - added physics to key, so it now falls when the secret compartment is set inactive

--- RestEasy/Assets/Scripts/BookshelfPuzzle.cs
+++ RestEasy/Assets/Scripts/BookshelfPuzzle.cs
@@ -6,12 +6,26 @@ public class BookshelfPuzzle : MonoBehaviour
 {
     public int targetBooksToCollect =  5;
     public int booksCollected = 0;
+    public GameObject secretCompartment;
+    public GameObject puzzleKey;
+    public GameObject ground;
     bool isPuzzleCompleted = false;
 
     void Update() {
         if (!isPuzzleCompleted && targetBooksToCollect == booksCollected) {
             Debug.Log("Bookshelf puzzle done!");
             isPuzzleCompleted = true;
+            secretCompartment.SetActive(false);
+
+        }
+        else if (isPuzzleCompleted) {
+            Rigidbody keyRB = puzzleKey.GetComponent<Rigidbody>();
+            if (puzzleKey.transform.position.y > ground.transform.position.y) {
+                keyRB.isKinematic = false;
+            }
+            else {
+                keyRB.isKinematic = true;
+            }
         }
     }
     
