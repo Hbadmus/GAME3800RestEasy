728c1030f8b2c16e465502a3384f9357e64ef959
marcellalmiller <149106160+marcellalmiller@users.noreply.github.com>
2025-04-20 08:54:08 -0400

    Doors
    All doors except for front door and secret door open/close when clicked

--- RestEasy/Assets/Scripts/GameManager.cs
+++ RestEasy/Assets/Scripts/GameManager.cs
@@ -104,12 +104,12 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        /*
         Invoke("OnPuzzleCompleted", 1f);  // First cutscene at 10 seconds
         Invoke("OnPuzzleCompleted", 25f);  // Second cutscene at 60 seconds
         Invoke("OnPuzzleCompleted", 50f);  // Second cutscene at 60 seconds
         Invoke("OnPuzzleCompleted", 75f);  // Second cutscene at 60 seconds
-
-
+        */
     }
 
     // Method to track puzzle completion
